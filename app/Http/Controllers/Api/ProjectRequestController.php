e76115a67ab2fcca02f50f5ea6715ff8f2ae3a2d
karam27 <karamrafeek2001@gmail.com>
2025-06-22 11:57:51 +0300

    add project request

--- app/Http/Controllers/Api/ProjectRequestController.php
+++ app/Http/Controllers/Api/ProjectRequestController.php
@@ -3,22 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProjectRequestForm;
 use App\Mail\ProjectRequestMail;
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Mail;
 
 class ProjectRequestController extends Controller
 {
-    public function sendEmail(Request $request)
+    public function sendEmail(ProjectRequestForm $request)
     {
-        $validated = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email',
-            'phone' => 'required|string',
-            'type' => 'required|string',
-            'details' => 'required|string',
-        ]);
+        $validated = $request->validated();
+
         Mail::to('test@example.com')->send(new ProjectRequestMail($validated));
+
         return response()->json(['message' => 'تم إرسال الطلب بنجاح']);
     }
 }
